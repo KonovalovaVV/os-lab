4b7b8f0e34a8bb49814a930b1b12de12471bf1f0
KonovalovaVV <lerakonovalova1003@gmail.com>
2020-03-15 18:49:37 +0300

    Add lab 1 for unix.

---
+++ Lab_1_Unix/Creator/Creator.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string.h>
+#include <fstream>
+
+using namespace std;
+
+struct employee
+{
+	int num;
+	char name[10];
+	double hours;
+};
+
+int main(int argc, char** argv)
+{
+	if (argc != 3)
+	{
+		cout << "ERROR: wrong number of	parameters\n";
+		return 1;
+	}
+	ofstream bin_file(argv[1], ios::binary | ios::out);
+	int employee_number = atoi(argv[2]);
+
+	employee tmp;
+	for (int i = 0; i < employee_number; i++)
+	{
+		cout << "Enter num:\n";
+		cin >> tmp.num;
+		cin.get();
+
+		cout << "Enter name:\n";
+		cin.getline(tmp.name, 10);
+
+		cout << "Enter hours:\n";
+		cin >> tmp.hours;
+
+		bin_file.write((char *)&tmp, sizeof(employee));
+	}
+
+	bin_file.close();
+}
---
+++ Lab_1_Unix/Main/Main.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fstream>
+#include <string>
+
+using namespace std;
+const int tiny_array = 4, med_array = 20, big_array = 100;
+
+void print_file(char* file_name, bool binary = false)
+{
+	cout << "\n\n" << file_name << ":\n";
+	ifstream fin;
+	if (binary)
+		fin.open(file_name, ios::binary);
+	else
+		fin.open(file_name);
+	while (!fin.eof())
+	{
+		string tmp;
+		getline(fin, tmp);
+		cout << tmp << endl;
+	}
+	fin.close();
+
+	cout << endl << endl;
+}
+
+int start_process_wait(char* const params[], char* process_name)
+{
+	pid_t pid = vfork();
+
+	if (pid == -1) {
+		fprintf(stderr, "Unable to fork\n");
+	}
+	else if (pid > 0)
+	{
+		int status;
+		waitpid(pid, &status, 0);
+		int exit_code = WEXITSTATUS(status);
+		if (exit_code)
+		{
+			printf("Exit code = %d\n", exit_code);
+			return 1;
+		}
+	}
+	else if (pid == 0) {
+		if (execv(process_name, params) == -1)
+		{
+			fprintf(stderr, "Unable to exec\n");
+			exit(pid);
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int main()
+{
+	char binary_file_name[med_array];
+	cout << "Enter name of bin file\n";
+	cin.getline(binary_file_name, med_array);
+
+	char num[tiny_array];
+	cout << "Enter num of records\n";
+	cin.getline(num, tiny_array);
+
+	char* creator_parameters[2];
+	creator_parameters[0] = binary_file_name;
+	creator_parameters[1] = num;
+	if (start_process_wait(creator_parameters, (char*)"Creator"))
+	{
+		return 1;
+	}
+	print_file(binary_file_name, true);
+
+	char report_file_name[med_array];
+	printf("\nEnter name of report file\n");
+	cin.getline(report_file_name, med_array);
+
+	char salary[tiny_array];
+	cout << "Enter salary \n";
+	cin.getline(salary, tiny_array);
+
+	char * reporter_parameters[3];
+	reporter_parameters[0] = binary_file_name;
+	reporter_parameters[1] = report_file_name;
+	reporter_parameters[2] = salary;
+	if (start_process_wait(reporter_parameters, (char*)"Reporter"))
+	{
+		return 1;
+	}
+	print_file(report_file_name, false);
+	return 0;
+}
